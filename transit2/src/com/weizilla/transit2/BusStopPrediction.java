ab5f2e2aad3f9c6decb56c8b715e91571f4e7449
Wei Yang <wei@weizilla.com>
2013-08-26 21:46:25 -0500

    added ui for look up

--- transit2/src/com/weizilla/transit2/BusStopPrediction.java
+++ transit2/src/com/weizilla/transit2/BusStopPrediction.java
@@ -3,6 +3,12 @@ package com.weizilla.transit2;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Activity for finding the predictions for a single bus stop
@@ -14,12 +20,24 @@ import android.view.View;
 public class BusStopPrediction extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         this.setContentView(R.layout.bus_prediction);
     }
 
     public void lookupPredictions(View view)
     {
-        // do prediction look up here
+        EditText busStopIdInput = (EditText) findViewById(R.id.uiBusStopIDInput);
+        String busStopId = busStopIdInput.getText().toString();
+
+        List<String> predictions = retrievePredictions(busStopId);
+        ArrayAdapter<String> predictionsAdapter
+                = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, predictions);
+
+        ListView uiPredictionsDisplay = (ListView) findViewById(R.id.uiPredictionList);
+        uiPredictionsDisplay.setAdapter(predictionsAdapter);
+    }
+
+    private List<String> retrievePredictions(String busStopId)
+    {
+        return Collections.singletonList(busStopId);
     }
 }
