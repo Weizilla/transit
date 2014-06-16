4fe25a90a9aa471dae43184acdd7a3b2d6b1326f
Wei Yang <wei@weizilla.com>
2014-06-15 21:31:57 -0500

    fix dependency

---
+++ lib-favorites/src/main/java/com/weizilla/transit/favorites/SqliteFavoriteStore.java
@@ -0,0 +1,22 @@
+package com.weizilla.transit.favorites;
+
+import com.weizilla.transit.bus.data.Route;
+
+import java.util.Collection;
+
+public class SqliteFavoriteStore implements BusFavoriteStore
+{
+    @Override
+    public void saveFavorite(Route route)
+    {
+        //TODO Auto-generated
+
+    }
+
+    @Override
+    public Collection<Route> getFavoriteRoutes()
+    {
+        //TODO Auto-generated
+        return null;
+    }
+}
