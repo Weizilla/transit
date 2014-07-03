c100b96399f01dc2151434d2994fe83f97a045f0
Wei Yang <wei@weizilla.com>
2014-07-02 20:16:47 -0500

    refactor bus data stream source test

--- lib-transit/src/test/java/com/weizilla/transit/bus/source/stream/BusInputStreamProviderStub.java
+++ lib-transit/src/test/java/com/weizilla/transit/bus/source/stream/BusInputStreamProviderStub.java
@@ -1,19 +1,32 @@
 package com.weizilla.transit.bus.source.stream;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class BusInputStreamProviderStub implements BusInputStreamProvider
 {
     private InputStream stream;
 
-    public BusInputStreamProviderStub(InputStream stream)
-    {
-        this.stream = stream;
-    }
-
     @Override
     public InputStream getRoutes()
     {
         return stream;
+    }
+
+    public void setStreamFromResource(String resourceName)
+    {
+        stream = getClass().getResourceAsStream(resourceName);
+    }
+
+    public void closeStream()
+    {
+        try
+        {
+            stream.close();
+        }
+        catch (IOException e)
+        {
+            // ignore
+        }
     }
 }
--- lib-transit/src/test/java/com/weizilla/transit/bus/source/stream/StreamingBusDataSourceTest.java
+++ lib-transit/src/test/java/com/weizilla/transit/bus/source/stream/StreamingBusDataSourceTest.java
@@ -1,21 +1,37 @@
 package com.weizilla.transit.bus.source.stream;
 
 import com.weizilla.transit.bus.data.Route;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.io.InputStream;
 import java.util.Collection;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class StreamingBusDataSourceTest
 {
+    private BusInputStreamProviderStub streamProvider;
+    private StreamingBusDataSource source;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        streamProvider = new BusInputStreamProviderStub();
+        source = new StreamingBusDataSource(streamProvider);
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        streamProvider.closeStream();
+    }
+
     @Test
     public void readsRoutesFromXmlStream() throws Exception
     {
-        InputStream inputStream = getClass().getResourceAsStream("/getroutes.xml");
-        BusInputStreamProviderStub streamProvider = new BusInputStreamProviderStub(inputStream);
-        StreamingBusDataSource source = new StreamingBusDataSource(streamProvider);
+        streamProvider.setStreamFromResource("/getroutes.xml");
 
         Collection<Route> routes = source.getRoutes();
         assertNotNull(routes);
@@ -24,6 +40,9 @@ public class StreamingBusDataSourceTest
         Route first = routes.iterator().next();
         assertEquals("1", first.getId());
         assertEquals("Bronzeville/Union Station", first.getName());
+
+        streamProvider.closeStream();
     }
+
 
 }
