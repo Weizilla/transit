42dae847c454dcefeeedd2962553715e5155f026
Wei Yang <wei@weizilla.com>
2014-08-22 11:55:35 -0500

    fix build

--- favorites-android/src/main/java/com/weizilla/transit/favorites/SqliteFavoritesStore.java
+++ favorites-android/src/main/java/com/weizilla/transit/favorites/SqliteFavoritesStore.java
@@ -1,6 +1,8 @@
 package com.weizilla.transit.favorites;
 
+import com.weizilla.transit.bus.data.Direction;
 import com.weizilla.transit.bus.data.Route;
+import com.weizilla.transit.bus.data.Stop;
 
 import java.util.Collection;
 
@@ -15,6 +17,20 @@ public class SqliteFavoritesStore implements BusFavoritesStore
 
     @Override
     public Collection<String> getFavoriteRoutes()
+    {
+        //TODO Auto-generated
+        return null;
+    }
+
+    @Override
+    public void saveFavorite(Stop stop)
+    {
+        //TODO Auto-generated
+
+    }
+
+    @Override
+    public Collection<Integer> getFavoriteStops(String route, Direction direction)
     {
         //TODO Auto-generated
         return null;
