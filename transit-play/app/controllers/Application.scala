ebe8a180403fe6c780c559aa7b6cf1425b7d19f9
Wei Yang <wei@weizilla.com>
2014-11-26 10:05:59 -0500

    add api key

--- transit-play/app/controllers/Application.scala
+++ transit-play/app/controllers/Application.scala
@@ -1,9 +1,12 @@
 package controllers
 
+import com.typesafe.config.ConfigFactory
 import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
+  val apiKey = ConfigFactory.load("api-key.conf").getString("apiKey")
+
   def index = Action {
-    Ok(views.html.index("scala now"))
+    Ok(views.html.index(apiKey))
   }
 }
